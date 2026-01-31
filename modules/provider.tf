8f0339a1e88ad72575314db8f52a6f4e1d006ab1
Rahul Sangwan <sangwan5228@anits232gmail.onmicrosoft.com>
2026-01-31 20:33:12 +0530

    rg

--- modules/provider.tf
+++ modules/provider.tf
@@ -8,7 +8,7 @@ terraform {
   backend "azurerm" {
   resource_group_name   = "pr-store-group"
   storage_account_name  = "prstorageaccount01"
-  container_name        = "tfstate"
+  container_name        = "statefile"
   key                   = "terraform.tfstate"
 }
 }
