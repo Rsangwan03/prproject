8518dfb655387603baa9cfb32001b9ad1b89eee3
Rahul Sangwan <sangwan5228@anits232gmail.onmicrosoft.com>
2026-01-31 19:29:51 +0530

    master

--- modules/provider.tf
+++ modules/provider.tf
@@ -5,9 +5,16 @@ terraform {
       version = "4.58.0"
     }
   }
+  backend "azurerm" {
+  resource_group_name   = "pr-store-group"
+  storage_account_name  = "prstorageaccount01"
+  container_name        = "tfstate"
+  key                   = "terraform.tfstate"
+}
 }
 
 provider "azurerm" {
   features {}
   subscription_id = "9121cde7-0fe4-44ee-a48f-f7f53ee83c6c"
+  
 }
